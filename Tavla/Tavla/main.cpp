20f353fc0daff57b1159d0132f202b982d526164
Leonit Shabani <leonitshabani3@gmail.com>
2024-04-22 18:46:59 +0300

    Göver Tasarım
    Görevleri ve alanları ayrılmış. Lütfen en kısa zamanda başlayın.

--- Tavla/Tavla/main.cpp
+++ Tavla/Tavla/main.cpp
@@ -2,6 +2,27 @@
 #include "freeglut.h"
 #include <iostream>
 
+//DIKKAT: YAZDIGINIZ HER SEY ICIN YORUM KULLANIN. DIGERLERIN KODLARINI IYICE YORUM YAPMADAN DOKUNMAYIN. 
+//SADECE ALANLARINDA KODLARINI YAZIN
+
+//Global Degisken Alani
+
+
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//EREN NOKTA - gover: Tahta Tasarimi
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//BAHA YOLAL - gover: Tas Tasrimi
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//ERLINDI ISAJ - gorev: Zar Tasarimi 
+
+//-------------------------------------------------------------------------------------------------------------------------------
+//LEONIT SHABANI - gorev: Oyun Mantigi
+
+//-------------------------------------------------------------------------------------------------------------------------------
+
 int main(int argc, char** argv) {
 
     glutInit(&argc, argv);
