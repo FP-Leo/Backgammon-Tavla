b47bc2a4b239807d9156e57dc5f06b4c9d7dfa53
Leonit Shabani <leonitshabani3@gmail.com>
2024-04-24 19:33:26 +0300

    Logic Implementation 4
    Changes Base Collection logic

--- Tavla/Tavla/main.cpp
+++ Tavla/Tavla/main.cpp
@@ -22,10 +22,12 @@ int toMove = 0;
 
 int outsideWhite = 0;
 int whiteReadyToCollect = false;
+int baseWhite = 0;
 int collectedWhite = 0;
 
 int outsideBlack = 0;
 int blackReadyToCollect = false;
+int baseBlack = 0;
 int collectedBlack = 0;
 
 //-------------------------------------------------------------------------------------------------------------------------------
@@ -45,52 +47,65 @@ void startingPosition() {
     //Beyaz
     colorArray[23] = 0; colorArray[12] = 0; colorArray[7] = 0; colorArray[5] = 0;
     numberArray[23] = 2; numberArray[12] = 5; numberArray[7] = 3; numberArray[5] = 5;
+    baseWhite = 5;
 
     //Siyah
     colorArray[0] = 1; colorArray[11] = 1; colorArray[16] = 1; colorArray[18] = 1;
     numberArray[0] = 2; numberArray[11] = 5; numberArray[16] = 3; numberArray[18] = 5;
+    baseBlack = 5;
 }
 
 int makeMove(int currentIndex, int rolledNumber, int targetSquare) {
-
-}
-
-int checkBase() {
-    int lowerBound = 0;
-    int upperBound = 7;
-    if (toMove == 1){
-        lowerBound = 18;
-        upperBound = 24;
-    }
-    int stonesFound = 0;
-    for (int i = lowerBound; i < upperBound; i++) {
-        if (colorArray[i] == toMove) {
-            stonesFound += numberArray[i];
+    int result = checkMove(currentIndex, rolledNumber, targetSquare);
+    if (result == 1){
+        if (toMove == 0 && !whiteReadyToCollect && targetSquare <= 5 && targetSquare >= 0) {
+            baseWhite++;
+            if (baseWhite + collectedWhite == 13)
+                whiteReadyToCollect = true;
+        }
+        else if (toMove == 1 && !blackReadyToCollect && targetSquare >= 18 && targetSquare <= 23) {
+            baseBlack++;
+            if (baseBlack + collectedBlack == 13)
+                blackReadyToCollect = true;
         }
     }
-    if (stonesFound == 13)
-        return 1;
-    else
-        return 0;
+    return result;
 }
 
 int checkMove(int currentIndex, int rolledNumber, int targetSquare) {
-    if (colorArray[targetSquare] != toMove) {
+    if (toMove == 0 && targetSquare >= currentIndex)
+        return 0;
+    else if (toMove == 1 && targetSquare <= currentIndex)
+        return 0;
+    else if (colorArray[targetSquare] != toMove) {
         if (numberArray[targetSquare] > 1) {
             return 0;
         }
+        numberArray[currentIndex]--;
+        if (numberArray[currentIndex] == 0) {
+            colorArray[currentIndex] = -1;
+        }
         colorArray[targetSquare] = toMove;
         switch (toMove) {
-            case 0: outsideBlack++; blackReadyToCollect = false; break;
-            case 1: outsideWhite++; whiteReadyToCollect = false; break;
+            case 0: outsideBlack++; blackReadyToCollect = false; 
+                if (targetSquare >= 18 && targetSquare <= 23) {
+                    baseBlack--;
+                }
+                break;
+            case 1: outsideWhite++; whiteReadyToCollect = false; 
+                if (targetSquare <= 5 && targetSquare >= 0) {
+                    baseWhite--;
+                }
+                break;
         }
         return 1;
     }
     else if (targetSquare < 0) {
         if (toMove == 1)
             return 0;
-        if (checkBase() && rolledNumber >= currentIndex) {
+        if (whiteReadyToCollect && rolledNumber >= currentIndex) {
             collectedWhite++;
+            baseWhite--;
             return 1;
         }
         return 0;
@@ -98,8 +113,9 @@ int checkMove(int currentIndex, int rolledNumber, int targetSquare) {
     else if (targetSquare > 23) {
         if (toMove == 0)
             return 0;
-        if (checkBase() && rolledNumber >= currentIndex) {
+        if (blackReadyToCollect && rolledNumber >= currentIndex) {
             collectedBlack++;
+            baseBlack--;
             return 1;
         }
         return 0;
