ce915cfdc9d01e0ec45c08b8b7eb39bc7bb4e2a6
Leonit Shabani <leonitshabani3@gmail.com>
2024-04-24 10:59:03 +0300

    Logic Implementation 2

--- Tavla/Tavla/main.cpp
+++ Tavla/Tavla/main.cpp
@@ -14,15 +14,19 @@ int checkMove(int , int , int);
 //Global Degisken Alani
 
 //Leo
-// 1 degeri - Beyaz, 2 ise siyah temsil eder. Beyaz tarafi 0dan 5'e kadar olan alanda toplicaktir. Siyah ise 18dan 23'e kadar olan alanda.
+// 0 degeri - Beyaz, 1 ise siyah temsil eder. Beyaz tarafi 0dan 5'e kadar olan alanda toplicaktir. Siyah ise 18dan 23'e kadar olan alanda.
 int colorArray[24] = { 0 }; 
 int numberArray[24] = { 0 };
-// Kimin halmesidir, 1 - beyaz, 2 - siyah
-int toMove = 1;
-int collectedWhite = 0;
-int collectedBlack = 0;
+// Kimin halmesidir, 0 - beyaz, 1 - siyah
+int toMove = 0;
+
 int outsideWhite = 0;
+int whiteReadyToCollect = false;
+int collectedWhite = 0;
+
 int outsideBlack = 0;
+int blackReadyToCollect = false;
+int collectedBlack = 0;
 
 //-------------------------------------------------------------------------------------------------------------------------------
 //EREN NOKTA - gover: Tahta Tasarimi
@@ -39,19 +43,27 @@ int outsideBlack = 0;
 //Set up starting position
 void startingPosition() {
     //Beyaz
-    colorArray[23] = 1; colorArray[12] = 1; colorArray[7] = 1; colorArray[5] = 1;
+    colorArray[23] = 0; colorArray[12] = 0; colorArray[7] = 0; colorArray[5] = 0;
     numberArray[23] = 2; numberArray[12] = 5; numberArray[7] = 3; numberArray[5] = 5;
 
     //Siyah
-    colorArray[0] = 2; colorArray[11] = 2; colorArray[16] = 2; colorArray[18] = 2;
+    colorArray[0] = 1; colorArray[11] = 1; colorArray[16] = 1; colorArray[18] = 1;
     numberArray[0] = 2; numberArray[11] = 5; numberArray[16] = 3; numberArray[18] = 5;
 }
 
 int checkMove(int currentIndex, int rolledNumber, int targetSquare) {
-    if (colorArray[targetSquare] != toMove)
-        return 0;
+    if (colorArray[targetSquare] != toMove) {
+        if (numberArray[targetSquare] > 1) {
+            return 0;
+        }
+        colorArray[targetSquare] = !colorArray[targetSquare];
+        switch (toMove) {
+            case 0: outsideBlack++; blackReadyToCollect = false; break;
+            case 1: outsideWhite++; whiteReadyToCollect = false; break;
+        }
+    }
     else if (targetSquare < 0) {
-        // To be implemented
+        
         return 0;
     }
     else if (targetSquare > 23) {
